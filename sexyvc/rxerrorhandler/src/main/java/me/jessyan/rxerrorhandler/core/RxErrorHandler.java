f5eda09de654cab296534ade47fef69c66ca7959
liusheng156 <15605538903@163.com>
2017-06-07 22:18:21 +0800

    初始化完成

---
+++ sexyvc/rxerrorhandler/src/main/java/me/jessyan/rxerrorhandler/core/RxErrorHandler.java
@@ -0,0 +1,59 @@
+package me.jessyan.rxerrorhandler.core;
+
+import android.content.Context;
+
+import me.jessyan.rxerrorhandler.handler.ErrorHandlerFactory;
+import me.jessyan.rxerrorhandler.handler.listener.ResponseErroListener;
+
+import static me.jessyan.rxerrorhandler.utils.Preconditions.checkNotNull;
+
+/**
+ * Created by ls on 9/2/16 13:27
+ */
+public class RxErrorHandler {
+    public final String TAG = this.getClass().getSimpleName();
+    private ErrorHandlerFactory mHandlerFactory;
+
+    private RxErrorHandler(Builder builder) {
+        this.mHandlerFactory = builder.errorHandlerFactory;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public ErrorHandlerFactory getmHandlerFactory() {
+        return mHandlerFactory;
+    }
+
+    public static final class Builder {
+        private Context context;
+        private ResponseErroListener responseErroListener;
+        private ErrorHandlerFactory errorHandlerFactory;
+
+        private Builder() {
+        }
+
+        public Builder with(Context context) {
+            this.context = context;
+            return this;
+        }
+
+        public Builder responseErroListener(ResponseErroListener responseErroListener) {
+            this.responseErroListener = responseErroListener;
+            return this;
+        }
+
+        public RxErrorHandler build() {
+            checkNotNull(context,"context is required");
+            checkNotNull(responseErroListener,"responseErroListener is required");
+
+
+            this.errorHandlerFactory = new ErrorHandlerFactory(context, responseErroListener);
+
+            return new RxErrorHandler(this);
+        }
+    }
+
+
+}
