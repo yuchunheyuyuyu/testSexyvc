cea722d8a0a022f48acfc12740e379bf6181c75e
liusheng156 <15605538903@163.com>
2017-09-15 18:57:48 +0800

    修改bug中

---
+++ sexyvc/app/src/main/java/com/qtin/sexyvc/ui/bean/ToImproveBean.java
@@ -0,0 +1,13 @@
+package com.qtin.sexyvc.ui.bean;
+
+import com.qtin.sexyvc.ui.subject.bean.DataTypeInterface;
+/**
+ * Created by ls on 17/9/15.
+ */
+public class ToImproveBean implements DataTypeInterface {
+
+    @Override
+    public int getType() {
+        return DataTypeInterface.TYPE_TO_IMPROVE;
+    }
+}
